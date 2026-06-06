49a266b5b78aaa610e5123fdd8e298c5c4152f5e
Claudia Santos Silva <claudia.tss@outlook.com>
2026-06-06 13:13:12 -0300

    Criado StackNavigator

--- src/routes/StackNavigator.jsx
+++ src/routes/StackNavigator.jsx
@@ -6,4 +6,25 @@ import AuthScreen from "../screens/AuthScreen";
 import PerfilScreen from "../screens/PerfilScreen";
 import Routes from ".";
 
+const NativeStack = createNativeStackNavigator(); 
 
+export default function StackNavigator() {
+  return (
+    <NativeStack.Navigator
+      initialRouteName={Routes.HOME}
+    //   screenOptions={{ headerShown: false }}
+    >
+      <NativeStack.Screen
+        name={Routes.HOME}
+        component={MainPageScreen}        
+      />
+      <NativeStack.Screen name={Routes.PASSAGENS} component={PassagensScreen} />
+      <NativeStack.Screen
+        name={Routes.DETALHES}
+        component={DetalhesScreen}        
+      />
+      <NativeStack.Screen name={Routes.AUTH} component={AuthScreen} />
+      <NativeStack.Screen name={Routes.PERFIL} component={PerfilScreen} />
+    </NativeStack.Navigator>
+  );
+}
