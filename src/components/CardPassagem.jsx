0cc13b5e2e09568ea95c38d4382d4fe7ddc288dc
Claudia Santos Silva <claudia.tss@outlook.com>
2026-06-14 16:39:56 -0300

    View da Ida feito

--- src/components/CardPassagem.jsx
+++ src/components/CardPassagem.jsx
@@ -28,12 +28,23 @@ export default function CardPassagem({dados}){
                 <Text style={styles.titulo}>{ciaAerea}</Text>
             </View>
              <View style={styles.divInfo}>
+
                 {/* Ida */}
                 <View style={styles.infoIda}>
-
+                    <View style={styles.partida}>
+                        <Text style={styles.horario}>{horaPartidaIda}</Text>
+                        <Text style={styles.data}>{aeroPartidaIda} · {dataPartidaIda}</Text>
+                    </View>
+                    <FontAwesome name="plane" size={16} color="black" style={{ transform: [{ rotate: "-270deg" }] }} />
+                    <View style={styles.chegada}>
+                        <Text style={styles.horario}>{horaChegadaIda}</Text>
+                        <Text style={styles.data}>{aeroChegadaIda} · {dataChegadaIda}</Text>
+                    </View>
                 </View>
+
                 {/* Volta */}
                 <View style={styles.infoVolta}>
+                    
 
                 </View>
                 {/* Detalhes */}
