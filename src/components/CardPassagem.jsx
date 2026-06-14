244f548fcccf6fb0bec88d13750d8b92c285052e
Claudia Santos Silva <claudia.tss@outlook.com>
2026-06-14 16:16:05 -0300

    Propriedades que serão usadas nos Cards.

--- src/components/CardPassagem.jsx
+++ src/components/CardPassagem.jsx
@@ -1,9 +1,29 @@
 import { View, Text, Image, StyleSheet } from "react-native";
 
-export default function CardPassagem(){
+export default function CardPassagem({dados}){
+
+ const {
+    ciaAerea,
+    horaPartidaIda,
+    aeroPartidaIda,
+    dataPartidaIda,
+    horaChegadaIda,
+    aeroChegadaIda,
+    dataChegadaIda,
+    horaPartidaVolta,
+    aeroPartidaVolta,
+    dataPartidaVolta,
+    horaChegadaVolta,
+    aeroChegadaVolta,
+    dataChegadaVolta,
+    paradas,
+    valor,
+  } = dados;
+
     return(
         <View>
             
+
         </View>
     );
 }
