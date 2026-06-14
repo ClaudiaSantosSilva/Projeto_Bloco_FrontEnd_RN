a14f089d03014ee87cb55db85c3a8ce06a1b9585
Claudia Santos Silva <claudia.tss@outlook.com>
2026-06-14 18:14:54 -0300

    CardPassagem sem estilo. 2 incones incluídos no assests.

--- src/components/CardPassagem.jsx
+++ src/components/CardPassagem.jsx
@@ -1,9 +1,8 @@
 import { View, Text, Image, StyleSheet } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 
-export default function CardPassagem({dados}){
-
- const {
+export default function CardPassagem({ dados }) {
+  const {
     ciaAerea,
     horaPartidaIda,
     aeroPartidaIda,
@@ -21,48 +20,78 @@ export default function CardPassagem({dados}){
     valor,
   } = dados;
 
-    return(
-        <View style={styles.container}>
-            <View style={styles.cardLateral}>
-                <FontAwesome name="plane" size={20} color="white" style={{ transform: [{ rotate: "-270deg" }] }} />
-                <Text style={styles.titulo}>{ciaAerea}</Text>
-            </View>
-             <View style={styles.divInfo}>
-
-                {/* Ida */}
-                <View style={styles.infoIda}>
-                    <View style={styles.partidaIda}>
-                        <Text style={styles.horario}>{horaPartidaIda}</Text>
-                        <Text style={styles.data}>{aeroPartidaIda} · {dataPartidaIda}</Text>
-                    </View>
-                    <FontAwesome name="plane" size={16} color="black" style={{ transform: [{ rotate: "-270deg" }] }} />
-                    <View style={styles.chegadaIda}>
-                        <Text style={styles.horario}>{horaChegadaIda}</Text>
-                        <Text style={styles.data}>{aeroChegadaIda} · {dataChegadaIda}</Text>
-                    </View>
-                </View>
-
-                {/* Volta */}
-                <View style={styles.infoVolta}>
-                    <View style={styles.partidaVolta}>
-                        <Text style={styles.horario}>{horaPartidaVolta}</Text>
-                        <Text style={styles.data}>{aeroPartidaVolta} · {dataPartidaVolta}</Text>
-                    </View>
-                    <FontAwesome name="plane" size={16} color="black" style={{ transform: [{ rotate: "270deg" }] }} />
-                    <View style={styles.chegadaVolta}>
-                        <Text style={styles.horario}>{horaChegadaVolta}</Text>
-                        <Text style={styles.data}>{aeroChegadaVolta} · {dataChegadaVolta}</Text>
-                    </View>
-                </View>
-                
-                {/* Detalhes */}
-                <View style={styles.divDetalhes}>
-
-                </View>
-
-             </View>
-
-
+  return (
+    <View style={styles.container}>
+      <View style={styles.cardLateral}>
+        <FontAwesome
+          name="plane"
+          size={20}
+          color="white"
+          style={{ transform: [{ rotate: "-270deg" }] }}
+        />
+        <Text style={styles.titulo}>{ciaAerea}</Text>
+      </View>
+      <View style={styles.divInfo}>
+        {/* Ida */}
+        <View style={styles.infoIda}>
+          <View style={styles.partidaIda}>
+            <Text style={styles.horario}>{horaPartidaIda}</Text>
+            <Text style={styles.data}>
+              {aeroPartidaIda} · {dataPartidaIda}
+            </Text>
+          </View>
+          <FontAwesome
+            name="plane"
+            size={16}
+            color="black"
+            style={{ transform: [{ rotate: "-270deg" }] }}
+          />
+          <View style={styles.chegadaIda}>
+            <Text style={styles.horario}>{horaChegadaIda}</Text>
+            <Text style={styles.data}>
+              {aeroChegadaIda} · {dataChegadaIda}
+            </Text>
+          </View>
         </View>
-    );
+
+        {/* Volta */}
+        <View style={styles.infoVolta}>
+          <View style={styles.partidaVolta}>
+            <Text style={styles.horario}>{horaPartidaVolta}</Text>
+            <Text style={styles.data}>
+              {aeroPartidaVolta} · {dataPartidaVolta}
+            </Text>
+          </View>
+          <FontAwesome
+            name="plane"
+            size={16}
+            color="black"
+            style={{ transform: [{ rotate: "270deg" }] }}
+          />
+          <View style={styles.chegadaVolta}>
+            <Text style={styles.horario}>{horaChegadaVolta}</Text>
+            <Text style={styles.data}>
+              {aeroChegadaVolta} · {dataChegadaVolta}
+            </Text>
+          </View>
+        </View>
+
+        {/* Detalhes */}
+        <View style={styles.divDetalhes}>
+          <View style={styles.divBagagens}>
+            <Image
+              source={require("../src/assets/malas.png")}
+              style={styles.iconBagagem}
+            />
+            <Image
+              source={require("../src/assets/malas_mao.png")}
+              style={styles.iconBagagem}
+            />
+          </View>
+          <Text>{paradas} Parada</Text>
+          <Text style={styles.valor}>R${valor}</Text>
+        </View>
+      </View>
+    </View>
+  );
 }
