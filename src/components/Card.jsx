4692a05915c14e56470f4dba49d69796528cc5e7
Claudia Santos Silva <claudia.tss@outlook.com>
2026-06-15 21:59:33 -0300

    Estrutura do Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,17 +14,40 @@ export default function Card({
   const idDoCard = `carrossel-${idCarrossel}-card-${numeroCard}`;
 
   return (
-  <View style={styles.card_container} accessibilityLabel={idDoCard}>
-    <View style={styles.card_image}>
-        <Image
-          source={{ uri: imgemSrc }}
-          alt={titulo}
-          style={styles.image}
-        />
+    <View style={styles.card_container} accessibilityLabel={idDoCard}>
+      <View style={styles.card_image}>
+        <Image source={{ uri: imgemSrc }} alt={titulo} style={styles.image} />
+      </View>
+      <View style={styles.card_info}>
+        <View style={styles.card_titulo}>
+          <Text style={styles.titulo}>{titulo}</Text>
+        </View>
+
+        <View style={styles.card_subtitle}>
+          <FontAwesome name="building" size={16} color="black" />
+          <Text>{subtitulo}</Text>
+        </View>
+        <View style={styles.card_pontuacao}>
+          <FontAwesome name="star" size={16} color="gold" />
+          <Text>{pontuacao}</Text>
+          <Text style={styles.reviews}>(Reviews)</Text>
+        </View>
+        <View style={styles.card_options}>
+          <View style={styles.card_icone}>
+            <Image
+              source={{ uri: iconeAdicionar }}
+              style={{ width: 24, height: 24 }}
+            />
+          </View>
+          <View>
+            <MaterialIcons name="directions-car" size={20} color="black" />
+            <MaterialIcons name="directions" size={20} color="black" />
+          </View>
+        </View>
+        <View style={styles.card_valor}>
+          <Text>{valor}</Text>
+        </View>
+      </View>
     </View>
-    <View>
-        
-    </View>
-  </View>
   );
 }
