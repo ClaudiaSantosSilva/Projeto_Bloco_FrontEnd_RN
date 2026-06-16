2d0e2fad0cb65c7609764c08e0f224bc93c45843
Claudia Santos Silva <claudia.tss@outlook.com>
2026-06-15 21:04:05 -0300

    Componente Botão genérico

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Pressable, Text } from "react-native";
+
+export default function Button({ accessibilityLabel, onPress, children, style }) {
+  return (
+    <Pressable
+      accessibilityLabel={accessibilityLabel}
+      onPress={onPress}
+      style={style} 
+    >
+      <Text>{children}</Text>
+    </Pressable>
+  );
+}
