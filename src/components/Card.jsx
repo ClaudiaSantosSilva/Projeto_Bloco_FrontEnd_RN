722cc86a2e8413c94102610486d5536bd3041984
Claudia Santos Silva <claudia.tss@outlook.com>
2026-06-15 22:08:26 -0300

    Estilo prévio do Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 export default function Card({
   idCarrossel,
@@ -33,12 +33,12 @@ export default function Card({
           <Text style={styles.reviews}>(Reviews)</Text>
         </View>
         <View style={styles.card_options}>
-          <View style={styles.card_icone}>
+          {/* <View style={styles.card_icone}> */}
             <Image
               source={{ uri: iconeAdicionar }}
               style={{ width: 24, height: 24 }}
             />
-          </View>
+          {/* </View> */}
           <View>
             <MaterialIcons name="directions-car" size={20} color="black" />
             <MaterialIcons name="directions" size={20} color="black" />
@@ -51,3 +51,96 @@ export default function Card({
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  card_container: {
+    width: 342,
+    height: 134,
+    borderRadius: 32,
+    opacity: 1,
+    backgroundColor: "#d6e6da",
+    flexDirection: "row", 
+  },
+  card_image: {
+    width: 121.5,
+    height: 134,
+    borderRadius: 32,
+    opacity: 1,
+  },
+  image: {
+    width: 121.5,
+    height: 134,
+    borderRadius: 32,
+    opacity: 1,
+  },
+  card_info: {
+    width: 220.5,
+    height: 134,
+    marginTop: 19,
+  },
+  card_titulo: {
+    justifyContent: "flex-start",
+    width: 210.54,
+    height: 26,
+    opacity: 1,
+    marginLeft: 10.21,
+  },
+  titulo: {
+    fontSize: 16,
+    fontWeight: "600",
+    lineHeight: 16, // 100% do fontSize
+    color: "#000000",
+  },
+  card_subtitle: {
+    width: 180,
+    height: 17,
+    flexDirection: "row",
+    gap: 6, 
+    alignItems: "center",
+  },
+//   subtitle_text: {
+//     fontSize: 12,
+//     fontWeight: "400",
+//     lineHeight: 12,
+//     color: "#000",
+//   },
+  card_pontuacao: {
+    width: 180,
+    height: 17,
+    flexDirection: "row",
+    gap: 6,
+    alignItems: "center",
+    marginTop: 6,
+  },
+//   pontuacao_text: {
+//     fontSize: 12,
+//     fontWeight: "400",
+//     lineHeight: 12,
+//     color: "#000",
+//   },
+  reviews: {
+    textDecorationLine: "underline",
+  },
+  card_options: {
+    width: 100,
+    height: 17,
+    flexDirection: "row",
+    gap: 6,
+    marginLeft: 9.75,
+    marginTop: 4,
+  },
+  card_valor: {
+    width: 210.54,
+    height: 21,
+    marginTop: 4,
+    marginLeft: 9.75,
+    justifyContent: "flex-start",
+  },
+//   valor_text: {
+//     fontSize: 20,
+//     fontWeight: "500",
+//     lineHeight: 20,
+//     color: "#000000",
+//   },
+});
+
