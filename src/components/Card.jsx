080348fad9e75fef21e24d1eabc7f87cbae92868
Claudia Santos Silva <claudia.tss@outlook.com>
2026-06-15 21:43:31 -0300

    Imagem do card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,30 @@
 import { View, Text, Image } from "react-native";
 
-export default function Card(){
-    return(
-        <View>
-            
-        </View>
-    );
+export default function Card({
+  idCarrossel,
+  numeroCard,
+  titulo,
+  subtitulo,
+  pontuacao,
+  iconeAdicionar,
+  valor,
+  imgemSrc,
+  style,
+}) {
+  const idDoCard = `carrossel-${idCarrossel}-card-${numeroCard}`;
 
+  return (
+  <View style={styles.card_container} accessibilityLabel={idDoCard}>
+    <View style={styles.card_image}>
+        <Image
+          source={{ uri: imgemSrc }}
+          alt={titulo}
+          style={styles.image}
+        />
+    </View>
+    <View>
+        
+    </View>
+  </View>
+  );
 }
